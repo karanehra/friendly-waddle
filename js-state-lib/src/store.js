61b356068555a430e11364250d7e762f5310ce26
Karan Nehra <karanehra@gmail.com>
2020-03-11 23:21:28 +0530

    Completed store implementation

--- js-state-lib/src/store.js
+++ js-state-lib/src/store.js
@@ -4,7 +4,6 @@ export default class Store {
   constructor(params) {
     this.actions = {}
     this.mutations = {}
-    this.state = {}
     this.status = 'idle'
     this.events = new PubSubHandler()
 
@@ -14,5 +13,59 @@ export default class Store {
     if (params.hasOwnProperty('mutations')) {
       this.mutations = params.mutations
     }
+
+    let self = this
+
+    this.state = new Proxy(params.state || {}, {
+      set: function(state, key, value) {
+        state[key] = value
+        console.log(`Key set trap: ${key}: ${value}`)
+
+        self.events.publish('stateChange', self.state)
+
+        if (self.status !== 'mutation') {
+          console.warn(`You should use a mutation to set ${key}`)
+        }
+
+        self.status = 'resting'
+
+        return true
+      }
+    })
+  }
+
+  /**
+   * Redux-esque action dispatcher
+   * @param {string} actionKey The key of the action
+   * @param {any} payload Data payload
+   */
+  dispatch(actionKey, payload) {
+    if (typeof this.actions[actionKey] !== 'function') {
+      console.error(`Action "${actionKey} doesn't exist.`)
+      return false
+    }
+    console.groupCollapsed(`ACTION: ${actionKey}`)
+
+    this.status = 'action'
+
+    this.actions[actionKey](this, payload)
+
+    console.groupEnd()
+
+    return true
+  }
+
+  commit(mutationKey, payload) {
+    if (typeof self.mutations[mutationKey] !== 'function') {
+      console.log(`Mutation "${mutationKey}" doesn't exist`)
+      return false
+    }
+    this.status = 'mutation'
+
+    let newState = this.mutations[mutationKey](this.state, payload)
+
+    this.state = Object.assign(this.state, newState)
+
+    return true
   }
 }
